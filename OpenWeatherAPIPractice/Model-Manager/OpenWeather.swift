8b176367b459f93f215f16c756261c5f14724052
DongHee-Sin <voslr0601@naver.com>
2022-08-13 18:22:40 +0900

    UserDevice의 위치로 OpenWeather API 요청

--- OpenWeatherAPIPractice/Model-Manager/OpenWeather.swift
+++ OpenWeatherAPIPractice/Model-Manager/OpenWeather.swift
@@ -14,11 +14,13 @@ import SwiftyJSON
 typealias OpenWeatherCompletionHandler = (Weather) -> Void
 
 
+
 struct Weather {
     let temp: Double
     let humidity: Int
     let windSpeed: Double
 }
+
 
 
 class OpenWeatherAPIManager {
@@ -27,18 +29,18 @@ class OpenWeatherAPIManager {
     private init() {}
     
     
-    func requestOpenWeather(completion: @escaping OpenWeatherCompletionHandler) {
+    func requestOpenWeather(lat: Double, lon: Double, completion: @escaping OpenWeatherCompletionHandler) {
         
         let url = EndPoint.openWeatherEndPoint
         
         // HTTP Body : 실질적인 데이터 > Dictionary 형태로 입력
         let parameter: Parameters = [
-            "lat": "",
-            "lon": "",
+            "lat": "\(lat)",
+            "lon": "\(lon)",
             "appid": APIKeys.OpenWeatherKey
         ]
         
-        AF.request(url, method: .post, parameters: parameter).validate(statusCode: 200...500).responseData { response in
+        AF.request(url, method: .get, parameters: parameter).validate(statusCode: 200...500).responseData { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
@@ -48,10 +50,10 @@ class OpenWeatherAPIManager {
                     print("StatusCode : \(statusCode)")
                     return
                 }
-                
-                let tmep = json["temp"].doubleValue
-                let humidity = json["humidity"].intValue
-                let wind = json["wind"].doubleValue
+
+                let tmep = json["main"]["temp"].doubleValue
+                let humidity = json["main"]["humidity"].intValue
+                let wind = json["wind"]["speed"].doubleValue
                 
                 completion(Weather(temp: tmep, humidity: humidity, windSpeed: wind))
                 
--- OpenWeatherAPIPractice/OpenWeatherViewController.swift
+++ OpenWeatherAPIPractice/OpenWeatherViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class OpenWeatherViewController: UIViewController, CommonSetting {
 
@@ -25,7 +26,28 @@ class OpenWeatherViewController: UIViewController, CommonSetting {
     
     
     // MARK: - Propertys
+    let apiManager = OpenWeatherAPIManager.shared
+    
+    let locationManager = CLLocationManager()
+
+    var userLocation: (lat: Double, lon: Double)? {
+        didSet {
+            if let userLocation = userLocation {
+                apiManager.requestOpenWeather(lat: userLocation.lat, lon: userLocation.lon) { [weak self] weather in
+                    self?.updateLabel(with: weather)
+                }
+            }else {
+                apiManager.requestOpenWeather(lat: defaultLocation.lat, lon: defaultLocation.lon) { [weak self] weather in
+                    self?.updateLabel(with: weather)
+                }
+            }
+        }
+    }
+    
+    let defaultLocation = (lat: 37.517829, lon: 126.886270)
+    
     let dateFormatter = DateFormatter()
+    
     var currentDateString: String {
         let date = Date()
         return dateFormatter.string(from: date)
@@ -38,12 +60,18 @@ class OpenWeatherViewController: UIViewController, CommonSetting {
         super.viewDidLoad()
         
         configureInitialUI()
+        
+        apiManager.requestOpenWeather(lat: defaultLocation.lat, lon: defaultLocation.lon) { weader in
+            dump(weader)
+        }
     }
     
     
     
     // MARK: - Methods
     func configureInitialUI() {
+        locationManager.delegate = self
+        
         [tempLabel, humidityLabel, windLabel, commentLabel].forEach {
             guard let label = $0 else { return }
             label.clipsToBounds = true
@@ -69,5 +97,95 @@ class OpenWeatherViewController: UIViewController, CommonSetting {
     
     func setRadius(view: UIView) {
         view.layer.cornerRadius = 8
+    }
+    
+    
+    func updateLabel(with weather: Weather) {
+        tempLabel.text = "지금은 \(weather.temp)에요"
+        humidityLabel.text = "\(weather.humidity)% 만큼 습해요"
+        windLabel.text = "\(weather.windSpeed)m/s의 바람이 불어요"
+    }
+}
+
+
+
+
+// MARK: - 위치 관련 User Defined 메서드
+extension OpenWeatherViewController {
+    
+    func checkUserDeviceLocationServiceAuthorization() {
+        
+        guard CLLocationManager.locationServicesEnabled() else {
+            showRequestLocationServiceAlert()
+            return
+        }
+        
+        let authorizationStatus: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        }else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        checkUserCurrentLocationAuthorization(authorizationStatus)
+    }
+    
+    
+    func checkUserCurrentLocationAuthorization(_ status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            
+        case .denied, .restricted:
+            showRequestLocationServiceAlert()
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            print("Default")
+        }
+    }
+    
+    
+    func showRequestLocationServiceAlert() {
+        let requestLocationServiceAlert = UIAlertController(title: "위치 정보 이용", message: "위치 서비스를 사용할 수 없습니다.\n디바이스의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+        let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
+            }
+        }
+        let cancel = UIAlertAction(title: "취소", style: .default)
+        requestLocationServiceAlert.addAction(cancel)
+        requestLocationServiceAlert.addAction(goSetting)
+        
+        present(requestLocationServiceAlert, animated: true)
+    }
+}
+
+
+
+
+// MARK: - CLLocationManager Protocol
+extension OpenWeatherViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let coordinate = locations.last?.coordinate {
+            userLocation = (coordinate.latitude, coordinate.longitude)
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+    }
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkUserDeviceLocationServiceAuthorization()
     }
 }
